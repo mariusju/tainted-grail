8fc0a3d36bc0ce391f82ff4e5fe5446861605726
Marius Jurgelenas <mariusj@wix.com>
2019-11-24 14:06:04 +0200

    Updates

---
+++ precache-manifest.1f0a99b43cac7b126a0d5b96279eaa67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fd7f26dc27723cfd7e01eb650d177d",
+    "url": "/tainted-grail/index.html"
+  },
+  {
+    "revision": "6084e637740e38fce49a",
+    "url": "/tainted-grail/static/css/main.691f7ece.chunk.css"
+  },
+  {
+    "revision": "c1f8cf89e9777c436cba",
+    "url": "/tainted-grail/static/js/2.09607981.chunk.js"
+  },
+  {
+    "revision": "6084e637740e38fce49a",
+    "url": "/tainted-grail/static/js/main.23d98e3e.chunk.js"
+  },
+  {
+    "revision": "ae673507cb01fb67b7c5",
+    "url": "/tainted-grail/static/js/runtime-main.e63d22e3.js"
+  }
+]);
